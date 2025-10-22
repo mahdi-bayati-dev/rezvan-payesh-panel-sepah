19cdc9976854f315113e073a2c7f88965c5e57bf
mcoder <mcoder902@gmail.com>
2025-10-22 12:32:16 -0500

    update

--- back_end/app/Models/LeaveType.php
+++ back_end/app/Models/LeaveType.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
 
 class LeaveType extends Model
 {
-    use HasFactory;
+    use HasFactory,HasRecursiveRelationships;
 
     protected $fillable = [
         'name',
