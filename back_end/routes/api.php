0ffcd13d360c5937ebbca23d75caa530fde7a59f
mcoder <mcoder902@gmail.com>
2025-10-23 10:30:51 -0500

    fix bugs

--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\api\AttendanceLogController;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
