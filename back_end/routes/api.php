ee0902a48727bcfee7f9ffc515f9a12145e923ee
mcoder <mcoder902@gmail.com>
2025-10-24 15:07:44 -0500

    holiday function

--- back_end/routes/api.php
+++ back_end/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AttendanceLogController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\HolidayController;
 use App\Http\Controllers\Api\WorkGroupController;
 use App\Http\Controllers\Api\WorkPatternController;
 use Illuminate\Http\Request;
@@ -24,6 +25,12 @@ Route::middleware('auth:api')->group(function () {
     Route::apiResource('work-patterns', WorkPatternController::class);
 
     Route::apiResource('work-groups', WorkGroupController::class);
+
+    //Holiday
+    Route::get('/holidays', [HolidayController::class, 'index'])->name('holidays.index');
+    Route::post('/holidays', [HolidayController::class, 'store'])->name('holidays.store');
+    Route::delete('/holidays/{date}', [HolidayController::class, 'destroy'])->name('holidays.destroy');
+
 });
 
 
