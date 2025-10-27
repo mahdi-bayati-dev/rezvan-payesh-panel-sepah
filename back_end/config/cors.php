f33a57beccc7555bb4f1fc32d145a52665ff7788
mcoder <mcoder902@gmail.com>
2025-10-27 14:06:52 -0500

    fix cors

--- back_end/config/cors.php
+++ back_end/config/cors.php
@@ -22,6 +22,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'https://rezvan-payesh.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
