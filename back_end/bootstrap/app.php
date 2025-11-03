06319c44b7f3c64d2cacec6fb070c6851fb11bcb
mcoder <mcoder902@gmail.com>
2025-11-03 13:59:51 -0600

    AttendanceLog fix bugs

--- back_end/bootstrap/app.php
+++ back_end/bootstrap/app.php
@@ -17,9 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->api(CheckLicenseStatus::class);
-        $middleware->api(prepend: [
-            \Laravel\Passport\Http\Middleware\CreateFreshApiToken::class,
-        ]);
         $middleware->alias([
             'role' => RoleMiddleware::class,
             'permission' => PermissionMiddleware::class,
