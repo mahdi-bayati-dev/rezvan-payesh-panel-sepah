281ef85a5990f28551429235905bb62498a8a4f2
mcoder <mcoder902@gmail.com>
2025-11-18 15:54:45 +0330

    fix bugs

--- back_end/app/Models/LicenseKey.php
+++ back_end/app/Models/LicenseKey.php
@@ -6,13 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class LicenseKey extends Model
 {
-    //
     protected $fillable = [
         'installation_id',
         'status',
         'expires_at',
         'user_limit',
         'license_token',
+        'trial_payload_db'
 
+    ];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+        'user_limit' => 'integer',
     ];
 }
