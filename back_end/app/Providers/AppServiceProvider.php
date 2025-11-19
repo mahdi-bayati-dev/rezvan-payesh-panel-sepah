d191fb261c1f99c1b900780ac340c48cfee7b0c5
mcoder <mcoder902@gmail.com>
2025-11-19 20:57:25 +0330

    fix license

--- back_end/app/Providers/AppServiceProvider.php
+++ back_end/app/Providers/AppServiceProvider.php
@@ -5,11 +5,13 @@ namespace App\Providers;
 use App\Models\AttendanceLog;
 use App\Models\Employee;
 use App\Models\LeaveRequest;
+use App\Models\User;
 use App\Models\WorkGroup;
 use App\Observers\AttendanceLogObserver;
 use App\Observers\EmployeeObserver;
 use App\Observers\LeaveRequestObserver;
 use App\Observers\WorkGroupObserver;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -27,6 +29,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Gate::define('viewPulse', function (User $user)
+        {
+            return $user->hasRole('super_admin');
+        });
+
         Employee::observe(EmployeeObserver::class);
         WorkGroup::observe(WorkGroupObserver::class);
         AttendanceLog::observe(AttendanceLogObserver::class);
