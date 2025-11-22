566a786ac7bdbf206ec0c32ce743be8eb45a9516
mcoder <mcoder902@gmail.com>
2025-11-22 21:09:03 +0330

    fix dashboard bug

--- back_end/app/Http/Controllers/Api/DashboardController.php
+++ back_end/app/Http/Controllers/Api/DashboardController.php
@@ -71,8 +71,8 @@ class DashboardController extends Controller
                 $query->whereBetween('start_time', [$startOfMonth, $today])
                       ->orWhereBetween('end_time', [$startOfMonth, $today])
                       ->orWhere(function ($q) use ($startOfMonth, $today) {
-                          $q->where('start_date', '<', $startOfMonth)
-                            ->where('end_date', '>', $today);
+                          $q->where('start_time', '<', $startOfMonth)
+                            ->where('end_time', '>', $today);
                       });
             })
             ->count();
