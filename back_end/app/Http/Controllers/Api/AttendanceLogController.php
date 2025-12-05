ae9f92b9ed60215b2fa62e03afb03049600bc1e1
mcoder <mcoder902@gmail.com>
2025-12-05 13:03:03 +0330

    fix

--- back_end/app/Http/Controllers/Api/AttendanceLogController.php
+++ back_end/app/Http/Controllers/Api/AttendanceLogController.php
@@ -163,6 +163,8 @@ class AttendanceLogController extends Controller
                 if ($logTimestamp->lt($expectedEnd))
                 {
                     Log::info("the exit time is2 : " . $expectedEnd->diffInMinutes($logTimestamp));
+                    Log::info("the expectedEnd time is: " . $expectedEnd);
+                    Log::info("the logTimestamp is: " . $logTimestamp);
                     $diffInMinutes = $expectedEnd->diffInMinutes($logTimestamp);
 
                     if ($diffInMinutes > $floatingEnd)
