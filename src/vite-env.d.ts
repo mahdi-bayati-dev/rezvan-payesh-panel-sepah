60765b5fdfc00dcf3e0ff3e6db03b8c2ed5e58bb
Mahdi Bayati <mad.bayati@gmail.com>
2025-12-07 23:17:01 +0330

    deker

---
+++ src/vite-env.d.ts
@@ -0,0 +1,24 @@
+// vite-env.d
+/// <reference types="vite/client" />
+
+interface Window {
+  globalConfig: {
+    // Core
+    VITE_API_BASE_URL?: string;
+    VITE_STORAGE_BASE_URL?: string;
+    
+    // AI Services
+    VITE_API_BASE_AI_URL?: string;
+    VITE_AI_SERVICE_SECRET?: string;
+
+    // Pusher / Soketi
+    VITE_PUSHER_APP_KEY?: string;
+    VITE_PUSHER_APP_CLUSTER?: string;
+    VITE_PUSHER_HOST?: string;
+    VITE_PUSHER_PORT?: string;
+    VITE_PUSHER_SCHEME?: string;
+    
+    // Auth
+    VITE_AUTH_MODE?: string;
+  };
+}
