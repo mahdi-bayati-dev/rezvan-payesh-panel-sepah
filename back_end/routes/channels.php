8e0f010d33f824dda842a5a450f90802c38aefbd
mcoder <mcoder902@gmail.com>
2025-12-21 19:11:31 +0330

    sockets fixed

--- back_end/routes/channels.php
+++ back_end/routes/channels.php
@@ -9,13 +9,13 @@ Broadcast::channel('App.User.{id}', function ($user, $id)
     return (int) $user->id === (int) $id;
 });
 
-Broadcast::channel('l3-channel.{organizationId}', function (User $user, int $organizationId)
+Broadcast::channel('l3-channel.{organizationId}', function (User $user, $organizationId)
 {
     return $user->hasRole('org-admin-l3') &&
            (int) $user->organization_id === $organizationId;
 });
 
-Broadcast::channel('l2-channel.{organizationId}', function (User $user, int $organizationId)
+Broadcast::channel('l2-channel.{organizationId}', function (User $user, $organizationId)
 {
     return $user->hasRole('org-admin-l2') &&
            (int) $user->organization_id === $organizationId;
