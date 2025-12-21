50b47d8b25e48a0fa2a8671328958e746d7536eb
mcoder <mcoder902@gmail.com>
2025-12-21 19:32:12 +0330

    sockets fixed

--- back_end/routes/channels.php
+++ back_end/routes/channels.php
@@ -12,23 +12,14 @@ Broadcast::channel('App.User.{id}', function ($user, $id)
 Broadcast::channel('l3-channel.{organizationId}', function (User $user, $organizationId)
 {
     return $user->hasRole('org-admin-l3') &&
-           (int) $user->organization_id === $organizationId;
+           (int) $user->employee?->organization_id === $organizationId;
 });
 
 Broadcast::channel('l2-channel.{organizationId}', function (User $user, $organizationId)
 {
-    $orgIdInt = (int) $organizationId;
-    $userOrgId = (int) $user->organization_id;
+    return $user->hasRole('org-admin-l2') &&
+           (int) $user->employee?->organization_id === $organizationId;
 
-    $hasRole = method_exists($user, 'hasRole') ? $user->hasRole('org-admin-l2') : false;
-    \Log::info("Broadcast Debug L2:", [
-        'user_id' => $user->id ?? 'null',
-        'has_role' => $hasRole ? 'YES' : 'NO',
-        'user_org' => $userOrgId,
-        'req_org' => $orgIdInt,
-        'match' => ($userOrgId === $orgIdInt) ? 'YES' : 'NO'
-    ]);
-    return $hasRole && ($userOrgId === $orgIdInt);
 });
 
 Broadcast::channel('super-admin-global', function (User $user)
