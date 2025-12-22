7883febde41a38d9f668b483e3159ac8c876020f
mcoder <mcoder902@gmail.com>
2025-12-22 17:43:11 +0330

    تست امتحانی

--- back_end/app/Http/Middleware/CheckStatus.php
+++ back_end/app/Http/Middleware/CheckStatus.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Response as ResponseFacade;
 class CheckStatus
 {
 
-    private int $max_trial_days_allowed = 5;
+    private int $max_trial_days_allowed = 4;
 
     public function __construct(protected CheckSystem $licenseService)
     {
